901ff889409e547a3d6b9a8759b1357fb903e2ab
johncris09 <johncrismanabo9@gmail.com>
2026-02-26 20:30:57 +0800

    feat: replace AppLogoIcon with logo image in AppLogo component

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,13 @@
-import AppLogoIcon from './app-logo-icon';
-
+import logo from '../../image/logo.png';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md   text-sidebar-primary-foreground">
+                <img
+                    src={logo}
+                    alt="BRGY CSM Logo"
+                    className="size-7 rounded-md object-cover"
+                />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
