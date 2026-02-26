c658d2c23fa53357865c91ceaf0879df519999a4
johncris09 <johncrismanabo9@gmail.com>
2026-02-26 17:07:38 +0800

    fix: update User type to allow id as number or string

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -1,9 +1,17 @@
 export type User = {
-    id: number;
+    id: number | string;
     name: string;
     email: string;
     avatar?: string;
     email_verified_at: string | null;
+    firstname?: string;
+    middlename?: string;
+    lastname?: string;
+    FirstName?: string;
+    MiddleName?: string;
+    LastName?: string;
+    joidnum?: string;
+    JOIDNUM?: string;
     two_factor_enabled?: boolean;
     created_at: string;
     updated_at: string;
