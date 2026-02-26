d99c031e9b96ea7f527ee0605eeb6c38074409d5
johncris09 <johncrismanabo9@gmail.com>
2026-02-26 16:41:43 +0800

    feat: add payment database connection configuration

--- config/database.php
+++ config/database.php
@@ -64,6 +64,27 @@ return [
             ]) : [],
         ],
 
+
+        'payment_connection' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_PAYMENT_HOST', '127.0.0.1'),
+            'port' => env('DB_PAYMENT_PORT', '3306'),
+            'database' => env('DB_PAYMENT_DATABASE', 'laravel'),
+            'username' => env('DB_PAYMENT_USERNAME', 'root'),
+            'password' => env('DB_PAYMENT_PASSWORD', ''),
+            'unix_socket' => env('DB_PAYMENT_SOCKET', ''),
+            'charset' => env('DB_PAYMENT_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_PAYMENT_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
